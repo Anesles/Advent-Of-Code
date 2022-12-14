12d33e712ad9b4fb57a5de08e246807910b93fef
brumarti <bbmp2002@gmail.com>
2022-12-14 23:54:15 +0000

    Made day4_p1 and p2

--- day4_p2/main.c
+++ day4_p2/main.c
@@ -10,17 +10,26 @@ int	check_contains(char *h1, char *h2)
 	l1 = ft_atoi(ft_substr(h2, 0, ft_strchrr(h2, '-')));
 	l2 = ft_atoi(ft_substr(h2, ft_strchrr(h2, '-') + 1, ft_strlen(h2) - ft_strchrr(h2, '-') - 1));
 	ft_printf("H2 = %s;\nN1 = %d; N2 = %d;\n", h2, l1, l2);
-	if (t1 <= l1 && t2 >= l2)
+	if (l1 >= t1 && l1 <= t2)
 	{
-		ft_printf("H1 contains H2\n\n");
+		ft_printf("H2 overlaps H1\n\n");
 		return (1);
 	}
-	else if (l1 <= t1 && l2 >= t2)
+	else if (t1 >= l1 && t1 <= l2)
 	{
-		ft_printf("H2 contains H1\n\n");
+		ft_printf("H1 overlaps H2\n\n");
 		return (1);
 	}
-	ft_printf("None contains the other\n\n");
+	if (l2 <= t2 && l2 >= t1)
+	{
+		ft_printf("H2 overlaps H1\n\n");
+		return (1);
+	}
+	else if (t2 <= l2 && t2 >= l1)
+	{
+		ft_printf("H1 overlaps H2\n\n");
+		return (1);
+	}
 	return (0);
 }
 
